e505bd6962d6ccbbff00d65639c65f506a27b6d1
Sreeranjani <sreeranjani.s11@gmail.com>
2026-03-09 13:51:43 +0100

    Day 1 Python basics Completed

---
+++ day1_math_operations.py
@@ -0,0 +1,12 @@
+#Program 3 — Simple math
+a = 5
+b = 3
+
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("True Division", a/b)
+print("Floor Division", a//b)
+print("Reminder", a%b)
+print("Exponential", a**b)
+print("Negative value", -a)
---
+++ day1_temperature_converter.py
@@ -0,0 +1,4 @@
+#Program 5 — Temperature converter
+celcius = 21
+fahrenheit = (celcius * 9/5) + 32
+print("Fahrenheit:", fahrenheit)
